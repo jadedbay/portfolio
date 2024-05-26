3fcb4d56974ba72cc2e5209e6b02ec676c60df9e
jaded <jaded@jadeds-MBP.modem>
2024-05-26 21:27:29 +1000

    rows

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -8,9 +8,8 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <div class="min-h-screen flex flex-col justify-between">
-          <div class="flex-grow-[1.6]"></div>
-          <div class="relative h-1/2 w-screen flex justify-center items-center self-center">
+        <div class="min-h-screen grid">
+          <div class="row-start-2 relative w-screen flex justify-center items-center">
             <div class="w-1/2 border-r border-zinc-300 overflow-hidden">
               <span class="animate-title-slide-in float-right p-8 font-thin text-3xl sm:text-6xl xs:text-9vw bg-gradient-to-r from-pink-700 to-purple-700 text-transparent bg-clip-text">
                 jaded
@@ -29,20 +28,20 @@ class Home extends Component {
               ))}
             </div>
           </div>
-          <div class="flex-grow-[0.4]"></div>
-          <span
-            class="m-1/16 animate-bounce text-zinc-300 hover:text-[#b52f9c] hover:animate-none self-center"
-            onClick={() => {
-              window.scrollTo({
-                behavior: "smooth",
-                top: window.innerHeight,
-              });
-            }}
-          >
-            <IconCaretDown stroke={1} size={32} />
-          </span>
+          <div class="row-start-3 flex justify-center align-center">
+            <span
+              class="animate-bounce text-zinc-300 hover:text-[#b52f9c] hover:animate-none self-center"
+              onClick={() => {
+                document
+                  .getElementById("projects")
+                  ?.scrollIntoView({ behavior: "smooth" });
+              }}
+            >
+              <IconCaretDown stroke={1} size={32} />
+            </span>
+          </div>
         </div>
-        <div class="min-h-screen bg-zinc-700"></div>
+        <div id="projects" class="min-h-screen bg-zinc-700"></div>
       </div>
     );
   }
